36a893955a4d91235499823c74ca77aaa12f1334
Nathan Langione <nlangione@wayfair.com>
2021-07-28 15:10:58 -0400

    Functionally complete, need to fix css

--- users_app/views.py
+++ users_app/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Users
+def index(request):
+    context = {
+        "all_users": Users.objects.all()    
+    }
+    return render(request,'index.html', context)
 
-# Create your views here.
+def process(request):
+    if request.method=="POST":
+        for key, value in request.POST.items():
+            print('Key: %s' % (key) ) 
+            # print(f'Key: {key}') in Python >= 3.7
+            print('Value %s' % (value) )
+            # print(f'Value: {value}') in Python >= 3.7 
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        email = request.POST.get('email')
+        age = request.POST.get('age')
+        new_user = Users.objects.create(first_name=first_name,last_name=last_name, email_address=email, age=age)
+        new_user.save()
+    return redirect("/")
+
